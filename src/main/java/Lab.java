fca5434923f9fc93bacac4db5d1b1763eb1e81aa
Farhana Sultana <farhanasultanashaik285@gmail.com>
2025-05-20 13:07:38 +0530

    Fixed Lab.java for test detection

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -2,19 +2,18 @@ public class Lab {
     public boolean isEqual(int a, int b){
         return a == b;
     }
-
     public boolean greaterThan(int a, int b){
         return a > b;
     }
-
+    public boolean lessThan(int a, int b){
+        return a < b;
+    }
     public boolean greaterThanOrEqualTo(int a, int b){
         return a >= b;
     }
-
     public boolean lessThanOrEqualTo(int a, int b){
         return a <= b;
     }
-
     public boolean notEqualTo(int a, int b){
         return a != b;
     }
