367785b7b46377d7a2be79fed45b3508e9a3ec54
Farhana Sultana <farhanasultanashaik285@gmail.com>
2025-05-20 12:58:04 +0530

    Fix method names and file structure

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,25 +1,10 @@
 public class Lab {
-
     public boolean isEqual(int a, int b){
         return a == b;
     }
 
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is greater than b, false otherwise.
-     */
     public boolean greaterThan(int a, int b){
         return a > b;
-    }
-
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is less than b, false otherwise.
-     */
-    public boolean lessThan(int a, int b){
-        return a < b;
     }
 
     public boolean greaterThanOrEqualTo(int a, int b){
